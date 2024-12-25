005594110f77482823ce858d5d11f034a3821560
TAIDONG <50778359+zhangtaidong@users.noreply.github.com>
2024-12-26 01:25:44 +1100

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.experteeth.com.au"];
-pub const PUBLIC_RS_PUB_KEY: &str = "";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.mtone.com.au"];
+pub const PUBLIC_RS_PUB_KEY: &str = "1nlTQm4aMGWNjHvGSe0xyGySSMfUS1P5PZrhdmlVsLQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
